fa36c26cd5e3b4c55b8726e3b2a74d145f759a07
Jardel O, Duarte <jardelduarte594@gmail.com>
2019-07-27 14:20:11 -0300

    init template in django

---
+++ env/bin/simp_project/simp_project/first/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import template 
+
+def index(request):
+	return render(request, 'index.html')
+
+def pag(request):
+	return render(request, 'cadaster.html') 
+# Create your views here.
